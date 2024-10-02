6cf2f250f519f7a045e858a2aaef5eb6ca9ee998
Devrim Genc <144224875+devGnc@users.noreply.github.com>
2024-10-02 19:49:19 +0200

    ajout du fichier javascript

---
+++ main/code/main.js
@@ -0,0 +1,11 @@
+function pomodoro() {
+    alert("Fonction Pomodoro activée !");
+}
+
+function todoList() {
+    alert("Ouvre ta TODO List !");
+}
+
+function notes() {
+    alert("Accède à tes notes !");
+}
